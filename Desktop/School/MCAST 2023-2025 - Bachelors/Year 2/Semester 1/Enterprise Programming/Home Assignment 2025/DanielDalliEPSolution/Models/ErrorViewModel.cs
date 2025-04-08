97cca85ed3e270dca705c9feff3f52a94ad657fc
edmccool30 <danieldalli5000@gmail.com>
2025-04-08 17:42:40 +0200

    Add .vs folder to .gitignore and remove it from tracking

--- Desktop/School/MCAST 2023-2025 - Bachelors/Year 2/Semester 1/Enterprise Programming/Home Assignment 2025/DanielDalliEPSolution/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DanielDalliEPSolution.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
